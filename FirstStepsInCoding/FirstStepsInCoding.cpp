1f9d0265c3fec76dda8b21e030d0570401d452c9
mialskywalker <mialskywalker@gmail.com>
2022-10-05 21:36:57 +0300

    a

--- FirstStepsInCoding/FirstStepsInCoding.cpp
+++ FirstStepsInCoding/FirstStepsInCoding.cpp
@@ -3,15 +3,10 @@ using namespace std;
 
 int main()
 {
-	cout << 1 << endl;
-	cout << 2 << endl;
-	cout << 3 << endl;
-	cout << 4 << endl;
-	cout << 5 << endl;
-	cout << 6 << endl;
-	cout << 7 << endl;
-	cout << 8 << endl;
-	cout << 9 << endl;
-	cout << 10 << endl;
+	int a;
+	int b;
+	cin >> a;
+	cin >> b;
+	cout << a * b << endl;
 	return 0;
 }
